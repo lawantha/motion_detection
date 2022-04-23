13fb40fb63ea2ac28505d399b3e87143a99fe9a2
Lawantha Bandara <lawantha111@gmail.com>
2022-04-23 22:55:21 +0530

    project renamed

--- emotion_detection.py
+++ emotion_detection.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # load model
-model = load_model('trained_model_csv.h5')
+model = load_model('Models/trained_model_csv.h5')
 
 # cascade file for face detection
 face_haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
